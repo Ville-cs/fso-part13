9ac52b8580275f4f402a5f059001d79d07fbe0fa
Ville <ville-poikela@hotmail.com>
2026-07-06 20:52:17 +0300

    implements various routes to api/users

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,37 @@
-const router = require("express").Router();
-
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
 const { User } = require("../models");
 
-router.get("/", async (req, res) => {
+usersRouter.get("/", async (req, res) => {
   const users = await User.findAll();
   res.json(users);
 });
 
-router.post("/", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.json(user);
-  } catch (error) {
-    return res.status(400).json({ error });
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  if (password.length < 3 || username.length < 3) {
+    return response
+      .status(400)
+      .json({
+        error: "username and password must be at least 3 characters long",
+      })
+      .end();
   }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = User.build({
+    username,
+    name,
+    passwordHash,
+  });
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
 });
 
-router.get("/:id", async (req, res) => {
+usersRouter.get("/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id);
   if (user) {
     res.json(user);
@@ -25,4 +40,29 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-module.exports = router;
+usersRouter.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.status(200).end();
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+usersRouter.put("/:username", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.params.username,
+      },
+    });
+    user.username = req.body.username;
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = usersRouter;
