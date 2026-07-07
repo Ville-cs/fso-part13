084c474dfc992f75d2eb131155a5e0dadc1fd2d2
Ville <ville-poikela@hotmail.com>
2026-07-07 15:59:48 +0300

    adds GET api/authors endpoint to aggregate authors of blogs

---
+++ controllers/authors.js
@@ -0,0 +1,24 @@
+const authorRouter = require("express").Router();
+const { Blog } = require("../models");
+const { sequelize } = require("../util/db");
+const { fn, col } = require("sequelize");
+
+authorRouter.get("/", async (req, res) => {
+  try {
+    const authors = await Blog.findAll({
+      attributes: [
+        "author",
+        [fn("COUNT", col("id")), "blogs"],
+        [fn("SUM", col("likes")), "likes"],
+      ],
+      group: ["author"],
+      order: [[fn("SUM", col("likes")), "DESC"]],
+      raw: true,
+    });
+    res.status(200).json(authors);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = authorRouter;
