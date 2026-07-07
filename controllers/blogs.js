f27c7a045e8ccffb48b68fa19027f2c4e0fe0702
Ville <ville-poikela@hotmail.com>
2026-07-07 15:59:21 +0300

    query search now works for both titles and authors

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,9 +6,18 @@ const { Op } = require("sequelize");
 blogsRouter.get("/", async (req, res) => {
   const where = {};
   if (req.query.search) {
-    where.title = {
-      [Op.substring]: req.query.search,
-    };
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+    ];
   }
   try {
     const blogs = await Blog.findAll({
@@ -18,6 +27,7 @@ blogsRouter.get("/", async (req, res) => {
         attributes: ["name"],
       },
       where,
+      order: [["likes", "DESC"]],
     });
     res.status(200).json(blogs);
   } catch (error) {
